18d463939c517ce85a82aa90ba29c5a018193e03
HL_1995 <347142093@qq.com>
2019-11-11 17:43:30 +0800

    test file struct
    Signed-off-by: HL_1995 <347142093@qq.com>

---
+++ FPGA_ip_core/1.call-vivado-bmg/src/test_rom.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2019/11/11 17:11:11
+// Design Name: 
+// Module Name: test_rom
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module test_rom(
+
+    );
+endmodule
