239b943fa2d59426543c7dd6bf89d2b8a00fc54b
HL_1995 <347142093@qq.com>
2020-05-28 09:55:24 +0800

    update work log
    Signed-off-by: HL_1995 <347142093@qq.com>

--- FPGA_prj_I/copy_filestruct.bat
+++ FPGA_prj_I/copy_filestruct.bat
@@ -1,1 +1,1 @@
-xcopy E:\working_files\LearningNotes\FPGA_prj_I\FPGA_prj_module E:\working_files\LearningNotes\FPGA_prj_I\project_name /e
+xcopy E:\working_files\LearningNotes\FPGA_prj_I\FPGA_prj_module E:\working_files\LearningNotes\FPGA_prj_I\hl_project /e/q
